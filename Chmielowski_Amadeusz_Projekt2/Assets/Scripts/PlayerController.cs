2c90763ae79db27d82972e2c18f6832e8c12fd52
Amadeusz Chmielowski <amadeusz.chmielowski@gmail.com>
2020-10-14 15:35:22 +0200

    player

---
+++ Chmielowski_Amadeusz_Projekt2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movmentSpeed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(movmentSpeed, 0.0f, 0.0f, Space.World);
+        }
+    }
+}
