c8d470b5ef82580d324f2f86395c4ae581039c74
Amadeusz Chmielowski <amadeusz.chmielowski@gmail.com>
2020-10-14 18:02:23 +0200

    map

--- Chmielowski_Amadeusz_Projekt2/Assets/Scripts/PlayerController.cs
+++ Chmielowski_Amadeusz_Projekt2/Assets/Scripts/PlayerController.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float movmentSpeed = 0.1f;
+    public float movmentSpeed = 4.0f;
+    public float jumpForce = 6.0f;
+    private Rigidbody2D rigidbody;
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -17,7 +24,15 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(movmentSpeed, 0.0f, 0.0f, Space.World);
+            transform.Translate(movmentSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-movmentSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
+        }
+        else if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
 }
