65510a2555c74f0cb08c8995428cb85a235270a6
Amadeusz Chmielowski <amadeusz.chmielowski@gmail.com>
2020-11-11 16:19:59 +0100

    auto lvl

---
+++ Chmielowski_Amadeusz_Projekt2/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+
+    public static LevelGenerator instance;
+
+    public Transform levelStartPoint;
+
+    public List<LevelPieceBasic> levelPrefabs = new List<LevelPieceBasic>();
+    public List<LevelPieceBasic> pieces = new List<LevelPieceBasic>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        AddPiece();
+        AddPiece();
+    }
+
+    // Update is called once per frame
+    public void AddPiece()
+    {
+        int randomIndex = Random.Range(0, levelPrefabs.Count - 1);
+        LevelPieceBasic piece = (LevelPieceBasic)Instantiate(levelPrefabs[randomIndex]);
+        piece.transform.SetParent(this.transform, false);
+
+        if(pieces.Count < 1)
+        {
+            piece.transform.position = levelStartPoint.position;
+        }
+        else
+        {
+            piece.transform.position = pieces[pieces.Count - 1].exitPoint.position;
+        }
+        pieces.Add(piece);
+    }
+
+    public void RemoveOldestPiece()
+    {
+        if(pieces.Count > 1)
+        {
+            LevelPieceBasic oldestPiece = pieces[0];
+            pieces.RemoveAt(0);
+            Destroy(oldestPiece.gameObject);
+        }
+    }
+}
